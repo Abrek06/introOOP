2b706921b2a000b2eb8c6562d0301f9bbe2d9949
Abrek06 <ozkandemirel06@gmail.com>
2021-04-24 23:16:31 +0300

    OOpUpdate1

--- src/oopIntro/Main.java
+++ src/oopIntro/Main.java
@@ -5,43 +5,40 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		Product product1 = new Product();
-		product1.id=1;
-		product1.name="Lenovo 14";
-		product1.unitPrice=15000;
-		product1.detail="16 GB Ram";
+		Product product1 = new Product(1,"Lenovo 14",15000,"16 GB Ram");		
 		
-		Product product2 = new Product();
-		product2.id=1;
-		product2.name="Lenovo 15";
-		product2.unitPrice=16000;
-		product2.detail="32 GB Ram";
+		Product product2 = new Product(2,"Lenovo 15",16000,"32 GB Ram");
 		
-		Product product3 = new Product();
-		product3.id=1;
-		product3.name="Hp 15";
-		product3.unitPrice=10000;
-		product3.detail="8 GB Ram";
+		Product product3 = new Product(3,"Hp 15",10000,"8 GB Ram");
 		
+
+		Product[] products = {product1, product2, product3};
 		
-		
- 		
-		Product[] products = {
-			
-				product1, product2, product3
-				
-		};
-		
-			System.out.println("Ürün Adedi: "+products.length);
+			System.out.println("Ürün Adedi  : "+products.length);
 			System.out.println("**************************");
 		
 		for (Product product : products) {
-			System.out.println("Id        : "+product.id);
-			System.out.println("ürünAdý   : "+product.name);
-			System.out.println("ÜrünFiyatý: "+product.unitPrice);
-			System.out.println("Açýklama  : "+product.detail);
+			System.out.println("Id          : "+product.getId());
+			System.out.println("ürünAdý     : "+product.getName());
+			System.out.println("ÜrünFiyatý  : "+product.getUnitPrice());
+			System.out.println("Açýklama    : "+product.getDetail());
 			System.out.println("**************************");
 		}
+		
+		Category category1 = new Category(1,"Bilgisayar");
+		
+		Category category2 = new Category(2,"Ev/Bahçe");
+		
+		Category[] categories = {category1,category2};
+		
+		for (Category category : categories) {
+			System.out.println("Kategori Id :"+category.getId());
+			System.out.println("kategori Adý:"+category.getName());
+			System.out.println("**************************");
+		}
+		
 	}
+	
+		
 
 }
